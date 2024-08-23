b13b0a49efac44b9f4933c89df8daed9f0225bc7
xd009642 <danielmckenna93@gmail.com>
2024-08-23 12:10:41 +0100

    Plot the data!

--- scripts/plot_audio.py
+++ scripts/plot_audio.py
@@ -6,7 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--audio", "-a", help="audio file to plot")
-parser.add_argument("--vad", "-v", help="vad output json to plot")
+parser.add_argument("--input", "-i", help="test data file to plot")
 
 args = parser.parse_args() 
 
@@ -14,35 +14,60 @@ args = parser.parse_args()
 wav_obj = wave.open(args.audio, 'rb')
 
 speech_segments = []
+silence_samples = []
+speech_samples = []
 
-padding_samples = 0
-with open(args.vad) as f:
-    vad = json.load(f)
-    padding_samples = int(vad["padding_samples"])
-    for segment in vad["segments"]:
-        speech_segments.append((segment["start_ms"], segment["end_ms"]))
+with open(args.input) as f:
+    data = json.load(f)
+    start = None
+    end = None
+
+    vad = data["summary"][args.audio]
+    silence_samples = vad["current_silence_samples"]
+    speech_samples = vad["current_speech_samples"]
+    for segment in vad["transitions"]:
+        print(segment)
+        if "SpeechStart" in segment:
+            start = int(segment["SpeechStart"]["timestamp_ms"])
+            end = None
+            print("Grabbing start")
+        elif "SpeechEnd" in segment:
+            print("Grabbing end")
+            end = int(segment["SpeechEnd"]["timestamp_ms"])
+            if start is not None and end is not None:
+                print("I've got both")
+                speech_segments.append((start, end))
+                start = None
+                end = None
 
 
 sample_freq = wav_obj.getframerate()
-n_samples = wav_obj.getnframes() + padding_samples
+n_samples = wav_obj.getnframes()
 t_audio = n_samples/sample_freq
+
+duration = 1000*n_samples / sample_freq
+if start is not None and end is None:
+    print("Adding")
+    end = int(round(duration))
+    speech_segments.append((start, end))
+
+print(f"Segments: {speech_segments}")
 
 signal_wave = wav_obj.readframes(n_samples)
 signal_array = np.frombuffer(signal_wave, dtype=np.int16)
 
-if padding_samples > 0:
-    padding = np.zeros(padding_samples, dtype=np.int16)
-    signal_array = np.concatenate((signal_array, padding ))
-
 
 times = np.linspace(0, n_samples/sample_freq, num=n_samples)
 
-fig, ax = plt.subplots()
+fig, (ax, ax2) = plt.subplots(2)
 
 ax.plot(times, signal_array)
 
 ax.set(xlabel="Time (s)", ylabel="Signal", title="Audio")
 
+ax2.plot(silence_samples, label = "Current silence samples")
+ax2.plot(speech_samples, label = "Current speech samples")
+ax2.legend()
 
 fill_regions = [False] * len(signal_array)
 
